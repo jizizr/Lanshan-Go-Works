733395b2089f96099cc83045fc86f0e8f9ebb74f
Allen Joe <jizizr1234@gmail.com>
2023-11-27 21:32:58 +0800

    (fix) 增加必要的原子操作

--- concurrent/Peterson/plock.go
+++ concurrent/Peterson/plock.go
@@ -1,15 +1,19 @@
 package plock
 
+import (
+	"sync/atomic"
+)
+
 // PetersonLock 分层锁
 type PetersonLock struct {
-	level   []int
-	waiting []int
+	level   []int32
+	waiting []int32
 }
 
 func NewPetersonLock(numThreads int) *PetersonLock {
 	h := &PetersonLock{
-		level:   make([]int, numThreads),
-		waiting: make([]int, numThreads-1),
+		level:   make([]int32, numThreads),
+		waiting: make([]int32, numThreads-1),
 	}
 	for i := range h.level {
 		h.level[i] = -1
@@ -23,23 +27,23 @@ func NewPetersonLock(numThreads int) *PetersonLock {
 // Lock 尝试获取锁
 func (h *PetersonLock) Lock(i int) {
 	for l := 0; l < len(h.waiting); l++ {
-		h.level[i] = l
-		h.waiting[l] = i
+		atomic.StoreInt32(&h.level[i], int32(l))
+		atomic.StoreInt32(&h.waiting[l], int32(i))
 
-		for h.waiting[l] == i && h.existsOtherWithLevelGreaterOrEqual(i, l) {
+		for atomic.LoadInt32(&h.waiting[l]) == int32(i) && h.existsOtherWithLevelGreaterOrEqual(i, l) {
 		}
 	}
 }
 
 // Unlock 释放锁
 func (h *PetersonLock) Unlock(i int) {
-	h.level[i] = -1
+	atomic.StoreInt32(&h.level[i], -1)
 }
 
 // existsOtherWithLevelGreaterOrEqual 检查是否存在其他进程在更高或相同的层级等待
 func (h *PetersonLock) existsOtherWithLevelGreaterOrEqual(i, l int) bool {
 	for k := 0; k < len(h.level); k++ {
-		if k != i && h.level[k] >= l {
+		if k != i && atomic.LoadInt32(&h.level[k]) >= int32(l) {
 			return true
 		}
 	}
