05fd8918029558a1bc0f1414996dd06fa47fe853
Allen Joe <jizizr1234@gmail.com>
2023-11-28 16:04:22 +0800

    使用map+mutex替代sync.Map

--- concurrent/syncMap/syncMap.go
+++ concurrent/syncMap/syncMap.go
@@ -13,43 +13,57 @@ type entry struct {
 	ready chan struct{}
 }
 
-func newEntry() *entry {
+func newEntry(v interface{}) *entry {
 	return &entry{
+		value: v,
 		ready: make(chan struct{}),
 	}
 }
 
 type SyncMap struct {
-	data sync.Map
+	mu   sync.Mutex
+	data map[int]*entry
 }
 
 func NewSyncMap() *SyncMap {
-	return &SyncMap{}
+	return &SyncMap{
+		data: make(map[int]*entry),
+	}
 }
 
 // Get 方法尝试获取键 k 对应的值。如果该键不存在，
 // 它将阻塞直到 maxWaitingTime 时间，等待值被放入。
 // 如果在超时时间内找到了值，它返回该值；否则返回错误。
 func (m *SyncMap) Get(k int, maxWaitingTime time.Duration) (interface{}, error) {
-	actual, _ := m.data.LoadOrStore(k, newEntry())
-	ent := actual.(*entry)
+	m.mu.Lock()
+	ent, exists := m.data[k]
+	if !exists {
+		ent = &entry{ready: make(chan struct{})}
+		m.data[k] = ent
+	}
+	m.mu.Unlock()
 
 	select {
 	case <-ent.ready:
 		return ent.value, nil
-	case <-time.After(maxWaitingTime): //设置超时时间
-		return 0, errors.New("")
+	case <-time.After(maxWaitingTime):
+		return nil, errors.New("")
 	}
 }
 
 // Put 方法将键 k 的值设置为 v。如果该键对应的 entry 已经存在，
 // 它更新该 entry 的值并关闭 ready 通道，通知等待该键的所有 Get 操作。
-func (m *SyncMap) Put(k, v interface{}) {
-	actual, loaded := m.data.LoadOrStore(k, &entry{value: v, ready: make(chan struct{})})
-	ent := actual.(*entry)
-	if loaded {
-		ent.value = v
+func (m *SyncMap) Put(k int, v interface{}) {
+	m.mu.Lock()
+	ent, exists := m.data[k]
+	if !exists {
+		ent = newEntry(v)
+		m.data[k] = ent
+		m.mu.Unlock()
+		close(ent.ready)
+		return
 	}
-	//关闭通道，通知所有阻塞的 Get 操作
+	ent.value = v
+	m.mu.Unlock()
 	close(ent.ready)
 }
